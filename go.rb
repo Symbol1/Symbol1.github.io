eea73688fc7317c8d9142e34eb1c67aba545a208
Symbol1 <S>
2016-10-21 21:18:22 -0500

    grip as new approach

--- go.rb
+++
@@ -1,6 +0,0 @@
-# encoding: utf-8
-
-require 'github/markup'
-file=File.open('index.html', 'w')
-file.write(GitHub::Markup.render('README.md', File.read('README.md')))
-file.close
---
+++ go.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+grip README.md --title=index --export index.html
